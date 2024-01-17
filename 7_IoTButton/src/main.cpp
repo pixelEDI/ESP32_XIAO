4624d49e8bb9b3025e073b0cfc81720ec7d8f260
pixeledi <hello@pixeledi.eu>
2024-01-17 15:00:13 +0100

    iot button

---
+++ 7_IoTButton/src/main.cpp
@@ -0,0 +1,51 @@
+/*
+        _          _ ______ _____ _____
+       (_)        | |  ____|  __ \_   _|
+  _ __  ___  _____| | |__  | |  | || |
+ | '_ \| \ \/ / _ \ |  __| | |  | || |
+ | |_) | |>  <  __/ | |____| |__| || |_
+ | .__/|_/_/\_\___|_|______|_____/_____|
+ | |
+ |_|
+
+www.pixeledi.eu | https://linktr.ee/pixeledi
+IoT Button | V1.0 | 01.2023
+
+*/
+
+#include <Arduino.h>
+#include <ArduinoJson.h>
+#include "wifimqtt.h"
+
+unsigned long previousMillis = millis();
+
+void setup()
+{
+  Serial.begin(115200);
+
+  connectAP();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void loop()
+{
+  if (!client.connected())
+  {
+    reconnect();
+  }
+  if (!client.loop())
+    client.connect("ESP32IoTButton");
+
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= (1000 * 20))
+  {
+    previousMillis = currentMillis;
+    client.publish("office/iotbutton", "23");
+    delay(1000);
+    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_OFF);
+    esp_deep_sleep_start();
+    Serial.println("Going to Sleep zzZZZzzZZZ");
+  }
+}
