f14799b9cc3ea2de6c90d965e5ce0e9afd937328
pixeledi <hello@pixeledi.eu>
2024-06-04 13:41:06 +0200

    epaper erste ansteuerung

---
+++ 21_e-paper-erste-ansteuerung/2_eink_helloworld_ArduinoIDE.ino
@@ -0,0 +1,88 @@
+// GxEPD2_HelloWorld.ino by Jean-Marc Zingg
+//
+// Display Library example for SPI e-paper panels from Dalian Good Display and boards from Waveshare.
+// Requires HW SPI and Adafruit_GFX. Caution: the e-paper panels require 3.3V supply AND data lines!
+//
+// Display Library based on Demo Example from Good Display: https://www.good-display.com/companyfile/32/
+//
+// Author: Jean-Marc Zingg
+//
+// Version: see library.properties
+//
+// Library: https://github.com/ZinggJM/GxEPD2
+
+// Supporting Arduino Forum Topics (closed, read only):
+// Good Display ePaper for Arduino: https://forum.arduino.cc/t/good-display-epaper-for-arduino/419657
+// Waveshare e-paper displays with SPI: https://forum.arduino.cc/t/waveshare-e-paper-displays-with-spi/467865
+//
+// Add new topics in https://forum.arduino.cc/c/using-arduino/displays/23 for new questions and issues
+
+// see GxEPD2_wiring_examples.h for wiring suggestions and examples
+// if you use a different wiring, you need to adapt the constructor parameters!
+
+// uncomment next line to use class GFX of library GFX_Root instead of Adafruit_GFX
+//#include <GFX.h>
+
+#include <GxEPD2_BW.h>
+#include <GxEPD2_3C.h>
+#include <Fonts/FreeMonoBold9pt7b.h>
+
+// select the display class and display driver class in the following file (new style):
+#include "GxEPD2_display_selection_new_style.h"
+
+// or select the display constructor line in one of the following files (old style):
+#include "GxEPD2_display_selection.h"
+#include "GxEPD2_display_selection_added.h"
+
+// ------------------------- Verschiedene MCUs ------------------------------------- //
+// 1.54"
+//  ESP32 XIAO
+//GxEPD2_BW<GxEPD2_154_D67, GxEPD2_154_D67::HEIGHT> display(GxEPD2_154_D67(/*CS=*/ 5, /*DC=*/ 2, /*RST=*/ 3, /*BUSY=*/ 4)); // GDEH0154D67 200x200, SSD1681
+
+// ESP32 DevKit
+//GxEPD2_BW<GxEPD2_154_D67, GxEPD2_154_D67::HEIGHT> display(GxEPD2_154_D67(/*CS=*/ 5, /*DC=*/ 12, /*RST=*/ 14, /*BUSY=*/ 27)); // GDEH0154D67 200x200, SSD1681
+
+// Arduino UNO
+// #define MAX_DISPLAY_BUFFER_SIZE 800
+// #define MAX_HEIGHT(EPD) (EPD::HEIGHT <= MAX_DISPLAY_BUFFER_SIZE / (EPD::WIDTH / 8) ? EPD::HEIGHT : MAX_DISPLAY_BUFFER_SIZE / (EPD::WIDTH / 8))
+// GxEPD2_BW<GxEPD2_154_D67, MAX_HEIGHT(GxEPD2_154_D67)> display(GxEPD2_154_D67(/*CS=*/ SS, /*DC=*/ 5, /*RST=*/ 6, /*BUSY=*/ 7)); // GDEH0154D67 200x200, SSD1681
+
+// 2.13"
+// GxEPD2_BW<GxEPD2_213_B74, GxEPD2_213_B74::HEIGHT> display(GxEPD2_213_B74(/*CS=5*/ 5, /*DC=*/2, /*RST=*/3, /*BUSY=*/4)); // GDEM0213B74 128x250, SSD1680
+
+// 2.9"
+GxEPD2_3C<GxEPD2_290c, GxEPD2_290c::HEIGHT> display(GxEPD2_290c(/*CS=5*/ 5, /*DC=*/2, /*RST=*/3, /*BUSY=*/4)); // GDEW029Z10 128x296, UC8151 (IL0373)
+// ------------------------- Verschiedene MCUs Ende ------------------------------------- //
+
+const char HelloWorld[] = "Hello pixelEDI 2.13";
+
+void helloWorld()
+{
+  display.setRotation(1);
+  display.setFont(&FreeMonoBold9pt7b);
+  display.setTextColor(GxEPD_RED);
+  // int16_t tbx, tby; uint16_t tbw, tbh;
+  // display.getTextBounds(HelloWorld, 0, 0, &tbx, &tby, &tbw, &tbh);
+  // // center the bounding box by transposition of the origin:
+  // uint16_t x = ((display.width() - tbw) / 2) - tbx;
+  // uint16_t y = ((display.height() - tbh) / 2) - tby;
+  display.setFullWindow();
+  display.firstPage();
+  do
+  {
+    display.fillScreen(GxEPD_WHITE);
+    display.setCursor(20, 20);
+    display.print(HelloWorld);
+  }
+  while (display.nextPage());
+}
+
+void setup()
+{
+  //display.init(115200); // default 10ms reset pulse, e.g. for bare panels with DESPI-C02
+  display.init(115200, true, 2, false); // USE THIS for Waveshare boards with "clever" reset circuit, 2ms reset pulse
+  helloWorld();
+  display.hibernate();
+}
+
+void loop() {};
